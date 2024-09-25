668116bb06bb46f3466257ca07b72fa65f312fb0
zebeid <zebeid@uvic.ca>
2024-09-25 14:57:41 -0700

    Set up project structure

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <errno.h>
+
+#define MAX_ARGS 64
+#define MAX_LINE 1024
+#define PROMPT_SIZE 128
+
 

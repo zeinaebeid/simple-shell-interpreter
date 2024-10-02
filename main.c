f5b918dc9bdab67cfe757b66d1008b1fe9c478da
Zeina Ebeid <zebeid@uvic.ca>
2024-10-02 01:08:36 +0000

    Upload New File

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <limits.h>
+#include <errno.h>
+
+#define MAX_ARGS 64
+#define MAX_LINE 1024
+#define PROMPT_SIZE 128
